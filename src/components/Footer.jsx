e4651daf47c257227fc4532b26fd547e5892b19d
matheus1714 <matheussilvamartins1714@gmail.com>
2021-12-31 10:10:56 -0300

    start project interactive comments section main

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => 
+    <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Matheus Silva Martins Mota</a>.
+    </div>
